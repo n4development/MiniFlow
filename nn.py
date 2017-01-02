0dac67aa716ad587760a63f5f707387ce112c398
Nader Karass <n4development@gmail.com>
2017-01-01 23:23:22 -0500

    complete Miniflow

--- nn.py
+++ nn.py
@@ -5,8 +5,223 @@ If all goes well, this should work after you
 modify the Add class in miniflow.py.
 """
 
-from miniflow import *
 
+"""
+Check out the new network architecture and dataset!
+
+Notice that the weights and biases are
+generated randomly.
+
+No need to change anything, but feel free to tweak
+to test your network, play around with the epochs, batch size, etc!
+"""
+
+from sklearn.datasets import load_boston
+from sklearn.utils import shuffle, resample
+from Stochastic import *
+import numpy as np
+
+# Load data
+data = load_boston()
+X_ = data['data']
+y_ = data['target']
+
+# Normalize data
+X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
+
+n_features = X_.shape[1]
+n_hidden = 10
+W1_ = np.random.randn(n_features, n_hidden)
+b1_ = np.zeros(n_hidden)
+W2_ = np.random.randn(n_hidden, 1)
+b2_ = np.zeros(1)
+
+# Neural network
+X, y = Input(), Input()
+W1, b1 = Input(), Input()
+W2, b2 = Input(), Input()
+
+l1 = Linear(X, W1, b1)
+s1 = Sigmoid(l1)
+l2 = Linear(s1, W2, b2)
+cost = MSE(y, l2)
+
+feed_dict = {
+    X: X_,
+    y: y_,
+    W1: W1_,
+    b1: b1_,
+    W2: W2_,
+    b2: b2_
+}
+
+epochs = 10
+# Total number of examples
+m = X_.shape[0]
+batch_size = 11
+steps_per_epoch = m // batch_size
+
+graph = topological_sort(feed_dict)
+trainables = [W1, b1, W2, b2]
+
+print("Total number of examples = {}".format(m))
+
+# Step 4
+for i in range(epochs):
+    loss = 0
+    for j in range(steps_per_epoch):
+        # Step 1
+        # Randomly sample a batch of examples
+        X_batch, y_batch = resample(X_, y_, n_samples=batch_size)
+
+        # Reset value of X and y Inputs
+        X.value = X_batch
+        y.value = y_batch
+
+        # Step 2
+        forward_and_backward(graph)
+
+        # Step 3
+        sgd_update(trainables)
+
+        loss += graph[-1].value
+
+    print("Epoch: {}, Loss: {:.3f}".format(i+1, loss/steps_per_epoch))
+
+
+"""
+Test your network here!
+
+No need to change this code, but feel free to tweak it
+to test your network!
+
+Make your changes to backward method of the Sigmoid class in miniflow.py
+"""
+"""
+from Backpropagation import *
+import numpy as np
+
+X, W, b = Input(), Input(), Input()
+y = Input()
+f = Linear(X, W, b)
+a = Sigmoid(f)
+cost = MSE(y, a)
+
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2.], [3.]])
+b_ = np.array([-3.])
+y_ = np.array([1, 2])
+
+feed_dict = {
+    X: X_,
+    y: y_,
+    W: W_,
+    b: b_,
+}
+
+graph = topological_sort(feed_dict)
+forward_and_backward(graph)
+# return the gradients for each Input
+gradients = [t.gradients[t] for t in [X, y, W, b]]
+
+print(gradients)
+"""
+
+"""
+Test your MSE method with this script!
+
+No changes necessary, but feel free to play
+with this script to test your network.
+"""
+"""
+from miniflow_cost import *
+import numpy as np
+
+y, a = Input(), Input()
+cost = MSE(y, a)
+
+y_ = np.array([1, 2, 3])
+a_ = np.array([4.5, 5, 10])
+
+feed_dict = {y: y_, a: a_}
+graph = topological_sort(feed_dict)
+# forward pass
+forward_pass(graph)
+print(cost.value)
+"""
+
+"""
+from miniflow_sigmod import *
+import numpy as np
+
+inputs, weights, bias = Input(), Input(), Input()
+
+f = Linear(inputs, weights, bias)
+g = Sigmoid(f)
+
+x = np.array([[-1., -2.], [-1, -2]])
+w = np.array([[2., -3], [2., -3]])
+b = np.array([-3., -5])
+
+feed_dict = {inputs: x, weights: w, bias: b}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(g, graph)
+
+print(output)
+"""
+
+"""
+from miniflow_layer import *
+import numpy as np
+
+
+inputs, weights, bias = Input(), Input(), Input()
+
+f = Linear(inputs, weights, bias)
+
+x = np.array([[-1., -2.], [-1, -2]])
+w = np.array([[2., -3], [2., -3]])
+b = np.array([-3., -5])
+
+feed_dict = {inputs: x, weights: w, bias: b}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+print(output)
+"""
+
+"""
+from miniflow_linear import *
+x, y, z = Input(), Input(), Input()
+inputs = [x, y, z]
+
+weight_x, weight_y, weight_z = Input(), Input(), Input()
+weights = [weight_x, weight_y, weight_z]
+
+bias = Input()
+
+f = Linear(inputs, weights, bias)
+
+feed_dict = {
+	x: 6,
+	y: 14,
+	z: 3,
+	weight_x: 0.5,
+	weight_y: 0.25,
+	weight_z: 1.4,
+	bias: 2
+}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+print(output) # should be 12.7 with this example
+"""
+
+"""
+from miniflow import *
 x, y, z = Input(), Input(), Input()
 
 f = Add(x, y, z)
@@ -18,3 +233,4 @@ output = forward_pass(f, graph)
 
 # should output 19
 print("{} + {} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
+"""
